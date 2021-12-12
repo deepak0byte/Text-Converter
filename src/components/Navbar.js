7c7bf8a6fec5ed53eae2390fafa46fa7e56f71d0
Deepak Kumar <deepak.kumar.mec20@itbhu.ac.in>
2021-12-12 16:46:04 +0530

    added clear function Feature

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,8 +16,8 @@ export default function Navbar(props) {
                         </li>
                     </ul>
                     <form className="d-flex">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success" type="submit">Search</button>
+                        <input className="form-control me-3" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-primary" type="submit">Search</button>
                     </form>
                 </div>
             </div>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -16,15 +16,22 @@ export default function TextForm(props) {
         console.log("On Change Handle.");
         setText(event.target.value)
     }
-    const [text, setText] = useState('Enter Your Text');
+
+    const Clear = () => {
+        let currentText = "";
+        setText(currentText);
+    }
+
+    const [text, setText] = useState('');
     return (
         <>
             <div className="mb-3">
                 <h3>{props.heading}</h3>
                 <textarea className="form-control" onChange={handleOnChange} value={text} id="box" rows="10"></textarea>
             </div>
-            <button className='btn btn-primary' onClick={convertUpperCase}>Convert UpperCase</button>
-            <button className='btn btn-primary' onClick={convertLowerCase}>Convert LowerCase</button>
+            <button className='btn1 btn-primary' onClick={convertUpperCase}><b>UpperCase</b></button>
+            <button className='btn1 btn-primary' onClick={convertLowerCase}><b>LowerCase</b></button>
+            <button className='btn1 btn-primary' onClick={Clear}><b>Clear</b></button>
         </>
     )
 }
